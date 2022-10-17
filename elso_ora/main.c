024d00c994416f40bbfe1f65ac5723b54d60ae00
Szántó Benjámin <61885011+vellt@users.noreply.github.com>
2022-10-17 21:04:41 +0200

    some new stuff

--- elso_ora/main.c
+++ elso_ora/main.c
@@ -3,12 +3,171 @@
 
 int main()
 {
-
     //feladat2();
-    feladat3();
+    //feladat3();
+    //feladat5();
+
+    gyakorlas6();
+    feladat7();
+    feladat8();
     return 0;
+}
+
+//teszt ugy össze álltani az eddig elhangzottak,
+
+feladat8(){
+    int i=12, *p;
+    p=&i;
+    *p=*p*2; //*=2;
+    printf("%d", i);
+
+    //pointeresk és tömbök
+    int t[5]={10,1,2,4,6};
+    p=t+1;
+    printf("p tomb merete: %d\n", sizeof(p)); //nem tudjuk megmondani vele h az eredeti tömb mekkora, csak az elemeit érjük el
+    printf("p tomb merete: %d\n", sizeof(t));
+
 
 }
+
+feladat7(){
+    int jegyek[10]={5,1,3,5,2,3,3,5,5,2};
+    int n=sizeof(jegyek)/sizeof(jegyek[0]);
+
+    //1
+    for(int i=0;i<n && jegyek[i]!=1;i++);
+    printf("Volt-e bukas? %s",i!=n?"Igen\n":"Nem\n");
+
+    //2a
+    for(int i=0;i<n && jegyek[i]!=5;i++);
+    printf("jeles volt-e az osztaly? %s",i!=n?"Igen\n":"Nem\n");
+
+    //2b
+    int darab=0;
+    for(int i=0;i<n;i++)if(jegyek[i]==5)darab++;
+    printf("Hany jeles volt az osztalyban? %d\n",darab);
+
+    //programozási programok, rendezések(teljes cserés rednezés)
+
+    //redezes novekvo sorban
+    int tomb[]={1,2,46,5,3,6,10};
+    //teljes cseres rendezes;
+    int nn=sizeof(tomb)/sizeof(tomb[0]);
+    int ii=0;
+    int j=0;
+    for(ii=0;ii<n-1;ii++){
+        for(j=ii+1;j<nn;j++){
+            if(tomb[ii]>tomb[j]){
+                int c=tomb[ii];
+                tomb[ii]=tomb[j];
+                tomb[j]=c;
+            }
+        }
+    }
+    for(ii=0;ii<nn;ii++) printf("%d ",tomb[ii]);
+    printf("\n");
+
+    int x=123;
+    int *p;
+    //ráállítom a p mutatót az x cimére
+    p=&x;
+    //%p memória cim kiirására alkamas
+    printf("%p \n",&x); //az x memória cime
+    printf("%p \n",p); //az x memória cime
+    printf("%d \n",x);//az x értéke
+
+    //a p-n keresztul megváltoztatom az x értékeét
+    *p=20;
+    //a p cimén lávő érték: *p
+    printf("%d \n",x);//az x értéke
+
+    //
+
+}
+
+/** literációk ciklusok **/
+void gyakorlas6(){
+    //while, do.. while, for..
+
+
+}
+
+void feladat5(){
+    //1
+    printf("Add meg a szuletesi eved: ");
+    int year=0;
+    scanf("%d",&year);
+
+    if(year<1900 || year >2022){
+        printf("Nagyon kicsi vagy nagyon nagy az ev amit megadtal!");
+    }else{
+        int age=2022-year;
+        printf("%d eves vagy", age);
+        if(age>=18) printf("nagykoru");
+    }
+    printf("\n\n");
+
+    //2
+    printf("Add meg egy szamot");
+    int number=0;
+    scanf("%d", &number);
+    int number1=number/10;
+    int number2=number%10;
+    if(number>=1 && number<=99){
+        switch(number){
+            case 10: printf("tiz"); break;
+            case 20: printf("husz"); break;
+            case 30: printf("harminc"); break;
+            default: {
+                switch(number1){
+                    case 1: printf("tizen"); break;
+                    case 2: printf("huszon"); break;
+                    case 3: printf("harminc"); break;
+                    case 4: printf("negyven"); break;
+                    case 5: printf("otven"); break;
+                    case 6: printf("hatvan"); break;
+                    case 7: printf("hetven"); break;
+                    case 8: printf("nyolcvan"); break;
+                    case 9: printf("kilencven"); break;
+                }
+                switch(number2){
+                    case 1: printf("egy"); break;
+                    case 2: printf("ketto"); break;
+                    case 3: printf("harom"); break;
+                    case 4: printf("negy"); break;
+                    case 5: printf("ot"); break;
+                    case 6: printf("hat"); break;
+                    case 7: printf("het"); break;
+                    case 8: printf("nyolc"); break;
+                    case 9: printf("kilenc"); break;
+                }
+            }
+        }
+    }
+
+    //3
+    printf("Adja meg a harmomszog osszes oldalanak hosszat: ");
+    float a, b,c;
+    scanf("%f %f %f", &a, &b, &c);
+    //szerkesztheto
+    if(a < b + c && b < a + c && c < a + b) //< ellentéte >= és ellentéte vagy
+    {
+        printf("Szerkesztheto a haromszog");
+        //derekszogu-e
+        if(a * a + b * b == c * c ||
+           b * b + c * c == a * a ||
+           a * a + c * c == b * b)
+            printf("derekszogu harmoszog");
+        else
+            printf("nem derekszogu");
+    }
+    else printf("nem szerkesztheto!");
+
+}
+
+// https://mooc.nye.hu/
+// https://mooc.nye.hu/enrol/index.php?id=15
+// prog1C!
 
 void feladat3(){
     //1
@@ -34,15 +193,13 @@ void feladat3(){
     }
 
     for(int i=0;i<3;i++){
-        if(numbers[i]==max)print("{}")
+        //if(numbers[i]==max)print("");
     }
 
 }
-
-void vlmi()
-{
+void vlmi(){
 /*
-    int konveim; //4byte-os rekeszt let�rol, 32bit, 2^32, ideiglenesen mem�ria szem�ttel kezd
+    int konveim; //4byte-os rekeszt letárol, 32bit, 2^32, ideiglenesen memória szeméttel kezd
     string  adat;
     printf("%s", adat);
 
@@ -67,10 +224,9 @@ void feladat2(){
     printf("%d",result);
     printf("\n");
 }
-
 void faladat1(){
 
-    //els� feladat
+    //elsõ feladat
     //1
     char c ='C';
     printf("%c : %i\n", c, c);
--- gyakorlas/main.c
+++ gyakorlas/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "szilva.h"
 
 
@@ -17,8 +19,222 @@ int main()
     //kivalasztas();
     //megaszamlalas();
     //minMax();
-    minMaxIndex();
+    //minMaxIndex();
+    //kivalasztasTetetele();
+    //egyszeruCseresRendezes();
+    //rendezesMinINdexKivalasztasaval();
+    //buborekosRendezes();
+    //putsGets();
+    //matematikaiFuggvenyek();
+    //matematikaiFuggvenyek2();
+    //matematikaiFuggvenyek3();
+    karaterkezelosFuggvenyek();
+    karaterkezelosFuggvenyek2();
     return 0; //hiba kódja, nulla az jó, minden más rossz!
+}
+
+void karaterkezelosFuggvenyek2(){
+
+}
+
+void karaterkezelosFuggvenyek(){
+    /*
+    char a;
+    printf("Adj mge egy kjaraktert.\n");
+    scanf("%c",&a);
+    if(isdigit(a)){
+        printf("Ez egy szam.\n");
+    }else {
+        printf("EZ nem egy szam\n");
+    }
+    */
+    char mondat[100];
+    int i, db=0;
+    puts("Adj meg egy mondatot.");
+    gets(mondat);
+
+    printf("%d\n",sizeof(mondat)/sizeof(mondat[0])); //ez az egesz tömb hosszat adja meg
+    printf("%d\n",strlen(mondat)); //itt a tenyxlegesen használt karakterek db-számát adja meg
+
+    for(i=0;i<strlen(mondat);i++){
+        if(isspace(mondat[i])){
+            db++;
+        }
+    }
+
+    printf("A mondat %d szobol epul fel.\n",db+1); //szókozük száma +1 == a szavak számával
+
+}
+
+void matematikaiFuggvenyek3(){
+    double a;
+    printf("Addj meg egy radian erteket.\n");
+    scanf("%lf", &a);
+    printf("A szog szinusza: %lf\n", sin(a)); //cos(a)
+    printf("A szám termeszetes alapu logaritmusa: %lf",log(a));
+
+}
+
+void matematikaiFuggvenyek2(){
+    //pow doublet ad vissza! megjeleníteni igy tudom: %.0lf
+    //hatványozás
+    //pow(a,2);
+    double a;
+    puts("Adj meg egy szamot");
+    scanf("%lf",&a);
+    for(int i=0;i<=10;i++){
+        printf("%.0lf^%d: %.0lf\n", a, i, pow(a,i));
+    }
+    int e;
+    puts("Adj meg egy szamot: ");
+    scanf("%d",&e);
+    printf("%d\n", (int)pow(e,2));
+
+    //sqrt(a) gyökvonás
+    printf("%.0lf",sqrt(e));
+
+}
+
+/** #include<math.h> **/
+void matematikaiFuggvenyek(){
+     float a;
+     printf("Adj meg egy szamot\n");
+     scanf("%f",&a);
+     //a=round(a); //kerekit
+     //a= ceil(a); //felfelé kerekit mindent (a%a==0)?a:(int)a+1  5.0 abból 5 marad
+     //a= ceil(a) ---> a= (a-(int)a)!=0?(int)a+1:a;
+     //ceil ellentéte a floor mindent lefelé kerekít
+     //abszulút érték (nullûtól való távolsógot adja vissza) abs vagy float esetében fabs
+     a=floor(a);
+     printf("%.2f",a);
+}
+
+/** stringek bekerese és kiiratasa **/
+void putsGets(){
+
+/*
+    char alma[100];
+
+    printf("Adj meg egy mondatot.\n");
+    scanf("%[^\n]s", alma); //nem kell & mert mutató
+    printf("szeretem az %s\n", alma);
+
+*/
+    char alma[100];
+    puts("Adj meg egy mondatot.");
+    gets(alma);
+    printf("szeretem az %s\n", alma);
+}
+
+/** prog tetel **/
+void buborekosRendezes(){
+   int tomb[N];
+   for(int i=0;i<N;i++){
+    printf("Add meg a tomb %d. elemet: ", i);
+    scanf("%d", &tomb[i]);
+   }
+
+   for(int i=1; i<N; i++){
+    for(int j=N-1;j>=i;j--){
+        if(tomb[j-1]>tomb[j]){
+            int temp=tomb[j-1];
+            tomb[j-1]=tomb[j];
+            tomb[j]=temp;
+        }
+    }
+   }
+
+   printf("\n");
+   for(int i=0;i<N;i++) printf("%d ", tomb[i]);
+
+}
+
+/** prog tetel LEGHATEKONYABB **/
+void rendezesMinINdexKivalasztasaval(){
+    int tomb[N];
+    for(int i=0;i<N;i++){
+        printf("Add meg a tomb %d. elemet: ",i);
+        scanf("%d", &tomb[i]);
+    }
+    for(int i=0;i<N-1;i++){
+        int minindex=i;
+        for(int j=i+1;j<N;j++){
+            if(tomb[j]<tomb[minindex]){
+                minindex=j;
+            }
+        }
+        if(i!=minindex){
+            int temp=tomb[i];
+            tomb[i]=tomb[minindex];
+            tomb[minindex]=temp;
+        }
+    }
+
+    printf("\n");
+    for(int i=0;i<N;i++) printf("%d ", tomb[i]);
+}
+
+/** prog tetel **/
+void egyszeruCseresRendezes(){
+    printf("Add meg a tomb hosszat: ");
+    int n=0;
+    scanf("%d",&n);
+    int tomb[n];
+    for(int i=0;i<n;i++){
+        printf("Add meg a tomb %d. elemet: ", i);
+        scanf("%d", &tomb[i]);
+    }
+    printf("\nA tomb elmei:\t");
+    for(int i=0;i<n;i++) printf("%d\t", tomb[i]);
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i;j<n;j++){
+            if(tomb[i]<tomb[j]){
+                int temp=tomb[j];
+                tomb[j]=tomb[i];
+                tomb[i]=temp;
+            }
+        }
+    }
+
+    printf("\nA tomb elmei:\t");
+    for(int i=0;i<n;i++) printf("%d\t", tomb[i]);
+    for(int i=0;i<n-1;i++){
+        for(int j=i;j<n;j++){
+            if(tomb[i]>tomb[j]){
+                int temp=tomb[j];
+                tomb[j]=tomb[i];
+                tomb[i]=temp;
+            }
+        }
+    }
+
+    printf("\nA tomb elmei:\t");
+    for(int i=0;i<n;i++) printf("%d\t", tomb[i]);
+}
+
+/** programozasi tetel **/
+void kivalasztasTetetele(){
+    int tomb[N];
+    for(int i=0; i<N;i++){
+        printf("Adja meg a %d. elemet: ", i);
+        scanf("%d",&tomb[i]);
+    }
+    //5 nel nagyobbak
+    int tomb2[N];
+    int j=0;
+
+    for(int i=0;i<N;i++){
+        if(tomb[i]>5){
+            tomb2[j]=i;
+            j++;
+        }
+    }
+
+    //kiiratjuk
+    for(int i=0;i<j;i++){
+        printf("%d. elem:\t %d",tomb2[i], tomb[tomb2[i]]);
+    }
 }
 
 /** programozasi tetel **/
